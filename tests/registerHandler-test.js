94631f44028e020de8bb5eab2cae5ed35c8eb99d
Johan Öbrink <johan.obrink@24hr.se>
2012-10-24 21:19:39 +0200

    Verb sensitive handlers

--- tests/registerHandler-test.js
+++ tests/registerHandler-test.js
@@ -16,7 +16,11 @@ describe('handlers', function() {
       var handler = function(req, res) {};
       server.addHandler('/foo', handler);
 
-      expect(server.handlers['/foo']).to.equal(handler);
+      expect(server.handlers['/foo']['GET']).to.equal(handler);
+      expect(server.handlers['/foo']['POST']).to.equal(handler);
+      expect(server.handlers['/foo']['PUT']).to.equal(handler);
+      expect(server.handlers['/foo']['DELETE']).to.equal(handler);
+      expect(server.handlers['/foo']['PATCH']).to.equal(handler);
     });
 
     it('should remove a reference to a simple handler', function() {
@@ -24,17 +28,70 @@ describe('handlers', function() {
       var handler = function(req, res) {};
 
       server.addHandler('/foo', handler);
-      expect(server.handlers['/foo']).to.equal(handler);
+      expect(server.handlers['/foo']['GET']).to.equal(handler);
 
-      server.removeHandler('/foo', handler);
+      server.removeHandler('/foo');
       expect(server.handlers['/foo']).to.be.undefined;
     });
 
+    it('should save a reference to a handler+verb', function() {
+
+      var handler = function(req, res) {};
+      server.addHandler('/foo', 'POST', handler);
+
+      expect(server.handlers['/foo']['POST']).to.equal(handler);
+    });
+
+    it('should remove a reference to a handler+verb', function() {
+
+      var handler = function(req, res) {};
+
+      server.addHandler('/foo', 'GET', handler);
+      server.addHandler('/foo', 'POST', handler);
+
+      expect(server.handlers['/foo']['GET']).to.equal(handler);
+      expect(server.handlers['/foo']['POST']).to.equal(handler);
+
+      server.removeHandler('/foo', 'POST');
+
+      expect(server.handlers['/foo']['GET']).to.equal(handler);
+      expect(server.handlers['/foo']['POST']).to.be.undefined;
+    });
+
+    it('should save a reference to a handler+verbs', function() {
+
+      var handler = function(req, res) {};
+      server.addHandler('/foo', 'GET,POST', handler);
+
+      expect(server.handlers['/foo']['GET']).to.equal(handler);
+      expect(server.handlers['/foo']['POST']).to.equal(handler);
+      expect(server.handlers['/foo']['PUT']).to.be.undefined;
+    });
+
+    it('should remove a reference to a handler+verbs', function() {
+
+      var handler = function(req, res) {};
+
+      server.addHandler('/foo', 'GET,POST,PUT', handler);
+
+      expect(server.handlers['/foo']['GET']).to.equal(handler);
+      expect(server.handlers['/foo']['POST']).to.equal(handler);
+      expect(server.handlers['/foo']['PUT']).to.equal(handler);
+      expect(server.handlers['/foo']['DELETE']).to.be.undefined;
+
+      server.removeHandler('/foo', 'GET,POST');
+
+      expect(server.handlers['/foo']['GET']).to.be.undefined;
+      expect(server.handlers['/foo']['POST']).to.be.undefined;
+      expect(server.handlers['/foo']['PUT']).to.equal(handler);
+    });
+
     describe('calls to handlers', function() {
-      it('should call a matching handler', function(done) {
+      it('should call a handler for matching path', function(done) {
 
         var request = new http.IncomingMessage();
         request.url = '/foo';
+        request.method = 'GET';
 
         server.addHandler('/foo', function(req, res) {
           expect(req).to.equal(request);
@@ -42,6 +99,34 @@ describe('handlers', function() {
         });
         server.server.emit('request', request);
 
+      });
+
+      it('should call a handler for matching path+verb', function(done) {
+
+        var request = new http.IncomingMessage();
+        request.url = '/foo';
+        request.method = 'POST';
+
+        server.addHandler('/foo', 'POST', function(req, res) {
+          expect(req).to.equal(request);
+          done();
+        });
+        server.server.emit('request', request);
+
+      });
+
+      it('should not call handler for non matching verb', function(done) {
+
+        var request = new http.IncomingMessage();
+        request.url = '/foo';
+        request.method = 'PATCH';
+
+        server.addHandler('/foo', 'GET,POST,PUT,DELETE', function(req, res) {
+          expect(false, 'handler should not be called for PATCH').to.be.true;
+        });
+
+        server.server.emit('request', request);
+        setTimeout(done, 10);
       });
     });
   });
